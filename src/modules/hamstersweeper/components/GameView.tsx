f26f54cec01d59073eec3df7909538f3dd77f38d
Avantgarde95 <mathematicianscott@gmail.com>
2025-01-28 12:02:15 +0900

    [feat] Back to the start page
    * Click left arrow

--- src/modules/hamstersweeper/components/GameView.tsx
+++ src/modules/hamstersweeper/components/GameView.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 import BoardView from "@/modules/hamstersweeper/components/BoardView";
 import GameStore, { GameContext } from "@/modules/hamstersweeper/stores/GameStore";
 import UIStore, { UIContext } from "@/modules/hamstersweeper/stores/UIStore";
 import Header from "@/modules/hamstersweeper/components/Header";
 import TimerStore, { TimerContext } from "@/common/stores/TimerStore";
 import Footer from "@/modules/hamstersweeper/components/Footer";
+import allRoutes from "@/common/models/Routes";
 
 const difficultyMap: Record<
   string,
@@ -20,14 +23,21 @@ const difficultyMap: Record<
   hard: { boardWidth: 16, boardHeight: 12, mineCount: 26 },
 };
 
+const outerButtonStyle = "h-8 w-6 text-lg";
+
 interface GameViewProps {
   difficulty: string;
 }
 
 const GameView = ({ difficulty }: GameViewProps) => {
+  const router = useRouter();
   const uiStore = new UIStore();
   const timerStore = new TimerStore({ interval: 1000 });
   const gameStore = new GameStore(difficultyMap[difficulty] ?? difficultyMap.easy);
+
+  const handleClickBack = () => {
+    router.push(allRoutes.hamstersweeper.home);
+  };
 
   return (
     <UIContext.Provider value={uiStore}>
@@ -36,8 +46,10 @@ const GameView = ({ difficulty }: GameViewProps) => {
           <div className="flex h-full w-full flex-row items-start overflow-auto p-4">
             <div className="border-outset m-auto border-4 bg-[#C0C0C0] p-2 pt-0">
               <div className="flex w-full flex-row items-start">
-                <button className="mr-auto h-8 w-6">⬅️</button>
-                <button className="h-8 w-6">❓</button>
+                <button className={`${outerButtonStyle} mr-auto`} onClick={handleClickBack}>
+                  ⬅️
+                </button>
+                <button className={outerButtonStyle}>❓</button>
               </div>
               <Header />
               <BoardView />
