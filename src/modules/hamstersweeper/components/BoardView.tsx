13f950eecb56a36a69649bbcb972c4cea4670e68
Avantgarde95 <mathematicianscott@gmail.com>
2025-02-01 22:16:47 +0900

    [refactor] Avoid unnecessary re-rendering of the board
    * In BoardView, use boardWidth and boardHeight, instead of board

--- src/modules/hamstersweeper/components/BoardView.tsx
+++ src/modules/hamstersweeper/components/BoardView.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext } from "react";
+import { ReactNode, useContext } from "react";
 import { observer } from "mobx-react-lite";
 
 import { GameContext } from "@/modules/hamstersweeper/stores/GameStore";
@@ -9,17 +9,23 @@ import CellView from "@/modules/hamstersweeper/components/CellView";
 const BoardView = observer(() => {
   const gameStore = useContext(GameContext);
 
-  return (
-    <div className="border-inset flex flex-col items-start border-4">
-      {gameStore.board.map((row, y) => (
-        <div key={y} className="flex flex-row items-start">
-          {row.map((cell, x) => (
-            <CellView key={x} position={{ x, y }} />
-          ))}
-        </div>
-      ))}
-    </div>
-  );
+  const rows: Array<ReactNode> = [];
+
+  for (let y = 0; y < gameStore.boardHeight; y++) {
+    const row: Array<ReactNode> = [];
+
+    for (let x = 0; x < gameStore.boardWidth; x++) {
+      row.push(<CellView key={x} position={{ x, y }} />);
+    }
+
+    rows.push(
+      <div key={y} className="flex flex-row items-start">
+        {row}
+      </div>
+    );
+  }
+
+  return <div className="border-inset flex flex-col items-start border-4">{rows}</div>;
 });
 
 export default BoardView;
